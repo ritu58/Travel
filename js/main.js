52f91349e703dbd0c503679e519b89731a681055
ritu58 <rituranid72@gmail.com>
2026-08-08 21:15:23 +0600

    First Commit

---
+++ js/main.js
@@ -0,0 +1,103 @@
+(function () {
+	
+	'use strict';
+	
+	var contentWayPoint = function() {
+		var i = 0;
+
+		// $('.travel-section').waypoint( function( direction ) {
+
+			$('.animate-box').waypoint( function( direction ) {
+
+				if( direction === 'down' && !$(this.element).hasClass('animated-fast') ) {
+					
+					i++;
+
+					$(this.element).addClass('item-animate');
+					setTimeout(function(){
+
+						$('body .animate-box.item-animate').each(function(k){
+							var el = $(this);
+							setTimeout( function () {
+								var effect = el.data('animate-effect');
+								if ( effect === 'fadeIn') {
+									el.addClass('fadeIn animated-fast');
+								} else if ( effect === 'fadeInLeft') {
+									el.addClass('fadeInLeft animated-fast');
+								} else if ( effect === 'fadeInRight') {
+									el.addClass('fadeInRight animated-fast');
+								} else {
+									el.addClass('fadeInUp animated-fast');
+								}
+
+								el.removeClass('item-animate');
+							},  k * 200, 'easeInOutExpo' );
+						});
+						
+					}, 100);
+					
+				}
+
+			} , { offset: '85%' } );
+		// }, { offset: '90%'} );
+	};
+
+	var goToTop = function() {
+
+		$('.js-gotop').on('click', function(event){
+			
+			event.preventDefault();
+
+			$('html, body').animate({
+				scrollTop: $('html').offset().top
+			}, 500, 'easeInOutExpo');
+			
+			return false;
+		});
+
+		$(window).scroll(function(){
+
+			var $win = $(window);
+			if ($win.scrollTop() > 200) {
+				$('.js-top').addClass('active');
+			} else {
+				$('.js-top').removeClass('active');
+			}
+
+		});
+	
+	};
+
+	var counter = function() {
+		$('.js-counter').countTo({
+			 formatter: function (value, options) {
+	      return value.toFixed(options.decimals);
+	    },
+		});
+	};
+
+	var counterWayPoint = function() {
+		if ($('#travel-counter').length > 0 ) {
+			$('#travel-counter').waypoint( function( direction ) {
+										
+				if( direction === 'down' && !$(this.element).hasClass('animated') ) {
+					setTimeout( counter , 400);					
+					$(this.element).addClass('animated');
+				}
+			} , { offset: '90%' } );
+		}
+	};
+
+	var dateForm = function() {
+		$('#date-start').datepicker();
+	};
+
+	$(function(){
+		contentWayPoint();
+		goToTop();
+		counterWayPoint();
+		dateForm();
+	});
+
+
+}());
